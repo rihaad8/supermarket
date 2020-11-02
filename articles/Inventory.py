5de2abe4ceab46946564508c3d0c8d1c7b77af68
rihaad8 <rlieschefsky@googlemail.com>
2020-11-02 22:22:40 +0100

    fixed an issue where all the data was loaded as string type, resulting in several issues following this

--- articles/Inventory.py
+++ articles/Inventory.py
@@ -12,11 +12,12 @@ class Inventory(object):
     def addArticle(self, Article):
         idTaken = False
         for article in self.articleList:
-            if(article.get_id() == Article.get_id()):
+            if(article.id == Article.id):
                 idTaken = True
-        if(idTaken):
+        if(not idTaken):
+                self.articleList.append(Article)
+        else:
             print("ID [",Article.get_id(),"] is already taken! Please try to add another Article.")
-        else: self.articleList.append(Article)
 
     def saveListToFile(self, fileName):
         with open(fileName, 'w', newline='') as inventoryFile:
@@ -30,7 +31,7 @@ class Inventory(object):
             reader = csv.reader(inventoryFile)
             firstLine = True
             for row in reader:
-                if firstLine:
+                if(firstLine):
                     firstLine = False
                     continue
-                self.addArticle(Article.Article(row[0], row[1], row[2]))
+                self.addArticle(Article.Article(int(row[0]), str(row[1]), float(row[2])))
--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from articles import Inventory
 superMarketInventory = Inventory.Inventory()
 superMarketInventory.loadListFromFile("Inventory.csv")
 superMarketInventory.addArticle(Article.Article(12, "AbraKadabra", 99.99))
+superMarketInventory.addArticle(Article.Article(123, "Monster Energy", 1.99))
 superMarketInventory.saveListToFile("Inventory.csv")
